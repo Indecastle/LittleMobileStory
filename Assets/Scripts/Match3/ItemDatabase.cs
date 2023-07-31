7f3324722f0ce5a9e271c4de2da53eaa871ebecf
Savkov, Andrey <andred9991@gmail.com>
2023-07-31 18:09:26 +0300

    Added DOTween package | Added init Match3 game

---
+++ Assets/Scripts/Match3/ItemDatabase.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ItemDataBase
+{
+    public static Item[] Items { get; private set; }
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void Initialize() => Items = Resources.LoadAll<Item>("Match3/Items/");
+}
